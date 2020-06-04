db0f91e25ac6a57bd753b27e8aa96245756265e1
AfonsJr <lafonsojr@gmail.com>
2020-06-04 20:48:00 -0300

    show text from modal

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,26 @@
 const result = document.querySelector(".teste");
 const input = document.querySelector("input");
-
 // TODO texto priveniente de um input
-const texto =
+const newText = document.getElementById("myText");
+
+// Texto default
+let texto =
   "Ao contrário do que se acredita, Lorem Ipsum não é simplesmente um texto randômico. Com mais de 2000 anos, suas raízes podem ser encontradas em uma obra de literatura latina clássica datada de 45 AC. Richard McClintock, um professor de latim do Hampden-Sydney College na Virginia, pesquisou uma das mais obscuras palavras em latim, consectetur, oriunda de uma passagem de Lorem Ipsum, e, procurando por entre citações da palavra na literatura clássica, descobriu a sua indubitável origem. Lorem Ipsum vem das seções 1.10.32 e 1.10.33 do de Finibus Bonorum et Malorum (Os Extremos do Bem e do Mal), de Cícero, escrito em 45 AC. Este livro é um tratado de teoria da ética muito popular na época da Renascença. A primeira linha de Lorem Ipsum, Lorem Ipsum dolor sit amet... vem de uma linha na seção 1.10.32.";
+
+// Se existe texto na textarea
+if (newText.value) {
+  texto = newText.value;
+}
+
+// quebra o texto em um array e pega o tamanho
 const texto2Array = texto.split(" ");
 const textoLength = texto2Array.length - 1;
+
+// seta o tamanho do input ao tamanho do texto
 input.max = textoLength;
 
 let contador = 0;
+
 const timer = setInterval(() => {
   let word = texto2Array[contador];
   result.textContent = `${word}`;
